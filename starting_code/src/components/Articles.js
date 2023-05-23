e8ce7a55622ef363cccaa70ed19d8d20b7b7c777
tjupp <travisjupp@gmail.com>
2023-05-23 14:56:46 -0600

    implement useSearchParams hook

--- starting_code/src/components/Articles.js
+++ starting_code/src/components/Articles.js
@@ -2,17 +2,18 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { selectArticles, filterArticles } from "../features/articles/articlesSlice";
 import Search from "./Search";
-import {Link, NavLink} from 'react-router-dom';
+import { Link, NavLink, useSearchParams } from 'react-router-dom';
 
 // Import Link and useSearchParams from React Router
 
-export default function Articles () {
+export default function Articles() {
   const articles = useSelector(selectArticles);
 
   // Grab URLSearchParams object from useSearchParams hook
-  
+  const [ searchParams ] = useSearchParams();
+
   // Get the queryParams from object returned from useSearchParams and set to `title`
-  const title = '';
+  const title = searchParams.get('title');
 
   const filteredArticles = title ? filterArticles(title, articles) : Object.values(articles)
 
@@ -20,7 +21,7 @@ export default function Articles () {
     <main>
       <h1>Articles</h1>
       <ul>
-        { filteredArticles.map(article => (
+        {filteredArticles.map(article => (
           <li key={article.slug}>
             {/* Replace these a tags! */}
             <Link to={`${article.slug}`}>
--- starting_code/src/components/SignUp.js
+++ starting_code/src/components/SignUp.js
@@ -2,18 +2,18 @@ import React, { useState } from "react";
 import { signUp } from "../features/session/sessionSlice";
 import { useDispatch } from "react-redux";
 // import useNavigate
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-export default function SignUp () {
+export default function SignUp() {
   const [username, setUsername] = useState("");
   const dispatch = useDispatch();
 
-   // Grab the navigate function
-   const navigate = useNavigate();
+  // Grab the navigate function
+  const navigate = useNavigate();
 
   const handleSubmit = e => {
     e.preventDefault();
-    dispatch(signUp({username: username}));
+    dispatch(signUp({ username: username }));
     // imperatively redirect the user to /profile
     navigate("/profile");
   }
